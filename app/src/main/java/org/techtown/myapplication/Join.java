a55bf5721edc9851480c474f0c8607ca9fa535ec
mindy15963 <39722516+mindy15963@users.noreply.github.com>
2021-12-18 18:49:38 +0900

    5th Commit

--- app/src/main/java/org/techtown/myapplication/Join.java
+++ app/src/main/java/org/techtown/myapplication/Join.java
@@ -38,7 +38,7 @@ public class Join extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.join);
 
         ActionBar actionbar = getSupportActionBar();
         actionbar.hide(); //타이틀바 가리기
